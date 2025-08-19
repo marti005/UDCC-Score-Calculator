5720cfd2a7e0b01aaf27ac883c814c9378c7433d
Marti 005 <martipiris28@gmail.com>
2025-08-19 23:17:11 +0200

    First functional version

---
+++ src/challengelist.js
@@ -0,0 +1,435 @@
+import {TIERS, C_TYPE, GAME, R_TYPE} from './params.js';
+
+export const challengeList = [{
+    name: "Asgore", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Undertale, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Chapter 3 Fanmade Weather Duo", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Chapter 4: Angelic Altercation", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Dalv", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Deltarune Chapter 1 No Heal", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Heal, 
+    game: GAME.Deltarune, 
+    rtype: R_TYPE.FG
+}, {
+    name: "Deltarune Chapter 2 No Heal", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Heal, 
+    game: GAME.Deltarune, 
+    rtype: R_TYPE.FG
+}, {
+    name: "Giga Queen", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Deltarune, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Guardener", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Jevil", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Deltarune, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "King", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Deltarune, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Mad Dummy", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Undertale, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Mettaton EX", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Undertale, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Mike's Big Showdown No Item/No Healing ACT", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Item, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Muffet", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Undertale, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Neutral Martlet", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Papyrus", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Undertale, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Queen", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Deltarune, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Sans", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Undertale, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Spamton NEO", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Deltarune, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Titan", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Deltarune, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Toriel", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Undertale, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Trio's Squabble (OG)", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "TS!Underswap Asgore", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "TS!Underswap Pacifist Crossbones", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "TS!Underswap Pacifist Muffet", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Undertale Red Updated Pacifist By Zhazha", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Undyne (Neutral)", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Undertale, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Undyne The Undying", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Undertale, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Unofficial Disbelief Papyrus Phase 1 No Heal", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Heal, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Unofficial Disbelief Papyrus Phase 4 No Heal", 
+    tier: TIERS.Beginner, 
+    ctype: C_TYPE.No_Heal, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Asriel Phase 1", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Undertale, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "B-TTale ESPE Sans (Original) No Heal", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Heal, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Deltarune Chapter 1", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Deltarune, 
+    rtype: R_TYPE.FG
+}, {
+    name: "Deltarune Chapter 1+2 No Heal", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Heal, 
+    game: GAME.Deltarune, 
+    rtype: R_TYPE.FG
+}, {
+    name: "Deltarune: Frozen Heart Noelle (Sparing Ending)", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Heal, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Deltatraveler Paula and Ness No Heal", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Heal, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Deltatraveler Porky", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Deltatraveler Section 1", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.FG
+}, {
+    name: "DTLG Phase 1 No Heal", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Heal, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "DTLG Phase 2 No Item", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Item, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "DTLG Phase 3 No Item", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Item, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "DTLG Phase 4 No Heal", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Heal, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Dustbelief Papyrus by Ari", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Edge ZY Phase 1 No Heal", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Heal, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Feisty Four", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Genocide Asgore (Normal) No Heal", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Heal, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Genocide Snowdin Martlet", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Hammer of Justice Gerson", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Deltarune, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Neutral Axis", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Neutral El Bailador", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "PS!Outertale Asriel", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "PS!Outertale Darker Neutral Undyne", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "PS!Outertale Mettaton NEO", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "PS!Outertale Undyne the Undying", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Spamton Restitched", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "SPMY Sans Phase 1 No Heal", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Heal, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Starlo", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Tenna", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Deltarune, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "The Heaven No Heal", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Heal, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Underfell Asgore Insane Mode No Heal", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Heal, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Undertale Last Breath Phase 2 No Heal", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Heal, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Undertale Red Updated Genocide by ZhaZha", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Unofficial Disbelief Papyrus All Phases No Item", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Item, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Unofficial Disbelief Papyrus Phase 2 No Heal", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Heal, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Unofficial Disbelief Papyrus Phase 3 No Heal", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Heal, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Unofficial Disbelief Papyrus Phase 4", 
+    tier: TIERS.Intermediate, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Other, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "ERAM/Mantle", 
+    tier: TIERS.Advanced, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Deltarune, 
+    rtype: R_TYPE.Battle
+}, {
+    name: "Deltarune Chapter 2 Main Route", 
+    tier: TIERS.Expert, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Deltarune, 
+    rtype: R_TYPE.FG
+}, {
+    name: "Deltarune Chapter 2 Snowgrave Route", 
+    tier: TIERS.Master, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Deltarune, 
+    rtype: R_TYPE.FG
+}, {
+    name: "Undertale Genocide", 
+    tier: TIERS.Elite, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Undertale, 
+    rtype: R_TYPE.FG
+}, {
+    name: "Undertale Neutral", 
+    tier: TIERS.Mythic, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Undertale, 
+    rtype: R_TYPE.FG
+}, {
+    name: "Deltarune TRACABARTPEEG", 
+    tier: TIERS.Grandmaster, 
+    ctype: C_TYPE.No_Hit, 
+    game: GAME.Deltarune, 
+    rtype: R_TYPE.FG
+}]
---
+++ src/params.js
@@ -0,0 +1,15 @@
+export const TIERS = {
+    Beginner: 10, Intermediate: 30, Advanced: 50, Expert: 100, Master: 300, Elite: 500, Mythic: 750, Grandmaster: 1000
+}
+
+export const C_TYPE = {
+    No_Hit: "No Hit", No_Heal: "No Heal", No_Item: "No Item"
+}
+
+export const GAME = {
+    Undertale: "Undertale", Deltarune: "Deltarune", Other: "Other"
+}
+
+export const R_TYPE = {
+    Battle: 'Battle', FG: "Full Game Run"
+}
